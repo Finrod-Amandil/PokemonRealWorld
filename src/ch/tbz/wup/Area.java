a32cf0626a7293e7df5904fb86820efa56d801a6
Neria <Neria13@users.noreply.github.com>
2018-06-07 09:50:31 +0200

    Area and AreaType

---
+++ src/ch/tbz/wup/Area.java
@@ -0,0 +1,49 @@
+package ch.tbz.wup;
+
+import java.awt.Polygon;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Area {
+	private static List<Area> _allAreas = new ArrayList<Area>();
+	
+	public static Area add(Polygon bounds, String name, AreaType type) {
+		//Check if area with same name and type already exists.
+		for (Area area : _allAreas) {
+			if (area.getName().equals(name) && area.getType().equals(type)) {
+				area.addSection(bounds);
+				return area;
+			}
+		}
+		
+		//Area does not exist yet
+		return new Area(bounds, name, type);
+	}
+	
+	public static ArrayList<Area> getAllAreas() {
+		return new ArrayList<Area>(_allAreas);
+	}
+	
+	private String _name;
+	private List<Polygon> _sectionBounds;
+	private AreaType _type;
+	
+	private Area(Polygon bounds, String name, AreaType type) {
+		_sectionBounds = new ArrayList<Polygon>();
+		_sectionBounds.add(bounds);
+		_type = type;
+		_name = name;
+	} 
+	
+	public String getName() {
+		return _name;
+	}
+	
+	public AreaType getType() {
+		return _type;
+	}
+	
+	private void addSection(Polygon section) {
+		_sectionBounds.add(section);
+	}
+}
---
+++ src/ch/tbz/wup/AreaType.java
@@ -0,0 +1,15 @@
+package ch.tbz.wup;
+
+public enum AreaType {
+	CITY,
+	LAKE,
+	RIVER,
+	SHORE,
+	PARK,
+	FOREST,
+	INDUSTRY,
+	SPORTSFIELD,
+	BATHS,
+	HOSPITAL,
+	TRACKFIELD
+}
