f5226e6fccddc45ba00a06d804380fb098433997
Neria13 <tiakuran.996@gmail.com>
2018-07-07 21:56:03 +0200

    Added generic areas to kml and database

--- src/ch/tbz/wup/PokemonStarter.java
+++ src/ch/tbz/wup/PokemonStarter.java
@@ -2,15 +2,22 @@ package ch.tbz.wup;
 
 import java.util.List;
 
+import ch.tbz.wup.models.Area;
 import ch.tbz.wup.models.PokemonSpecies;
 import ch.tbz.wup.persistence.DbContext;
+import ch.tbz.wup.persistence.IDbContext;
 
 public class PokemonStarter {
 
 	public static void main(String[] args) {
+		IDbContext context = new DbContext();
+		List<Area> areas = context.getAllAreas();
+		for (Area area : areas) {
+			System.out.println("ID = " + area.getId() + " Name = " + area.getName() + " Type = " + area.getType().toString());
+		}
+		
 		StartUp.configure();
 		
-		DbContext context = new DbContext();
 		List<PokemonSpecies> all_species = context.getAllPokemon();
 		for (PokemonSpecies species: all_species) {
 			if (species.getType2() == null) {
