ffacb2e9657a502937f0ebd705bd9dfe2a42d3dd
Severin Zahler <severin.zahler@gmail.com>
2018-07-08 13:50:14 +0200

    Add AreaSection class to pre-generate section bounding boxes

--- src/ch/tbz/wup/models/AreaSection.java
+++ src/ch/tbz/wup/models/AreaSection.java
@@ -2,14 +2,16 @@ package ch.tbz.wup.models;
 
 import java.awt.Polygon;
 import java.awt.Rectangle;
+import java.io.Serializable;
 
-public class AreaSection {
+public class AreaSection implements Serializable {
 	
+	private static final long serialVersionUID = 1L;
 	public Polygon bounds;
 	public Rectangle boundingBox;
 	
 	public AreaSection(Polygon bounds) {
 		this.bounds = bounds;
-		boundingBox = bounds.getBounds();
+		boundingBox = bounds != null ? bounds.getBounds() : new Rectangle(0,0,0,0);
 	}
 }
